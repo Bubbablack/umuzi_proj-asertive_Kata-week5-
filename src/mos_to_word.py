c2a45b63f171a9cba13c706f93b8419e5855ead3
Bubblack <kevin.mntambo@gmail.com>
2020-06-09 20:19:12 +0200

    made sure my functions are short

--- src/mos_to_word.py
+++ src/mos_to_word.py
@@ -1,105 +1,26 @@
+from dictionary import dic
 
-code = input("please enter your moss code here")
+def decode(temp):
+    final = ""
+    for key in dic:
+        if temp == dic[key]:
+            final += key
+    return(final)  
 
-
-def decode(sentance):
-    scode = ""
-    if sentance == "/":
-        scode += " "
-    elif sentance == ".-":
-        scode += "a"
-    elif sentance == "-...":
-        scode += "b"
-    elif sentance == "-.-.":
-        scode += "c"
-    elif sentance == "-..":
-        scode += "d"
-    elif sentance == ".":
-        scode += "e"
-    elif sentance == "..-.":
-        scode += "f"
-    elif sentance == "--.":
-        scode += "g"
-    elif sentance == "....":
-        scode += "h"
-    elif sentance == "..":
-        scode += "i"
-    elif sentance == ".---":
-        scode += "j"
-    elif sentance == "-.-":
-        scode += "k"
-    elif sentance == ".-..":
-        scode += "l"
-    elif sentance == "--":
-        scode += "m"
-    elif sentance == "-.":
-        scode += "n"
-    elif sentance == "---":
-        scode += "o"
-    elif sentance == ".--.":
-        scode += "p"
-    elif sentance == "--.-":
-        scode += "q"
-    elif sentance == ".-.":
-        scode += "r"
-    elif sentance == "...":
-        scode += "s"
-    elif sentance == "-":
-        scode += "t"
-    elif sentance == "..-":
-        scode += "u"
-    elif sentance == "...-":
-        scode += "v"
-    elif sentance == ".--":
-        scode += "w"
-    elif sentance == "-..-":
-        scode += "x"
-    elif sentance == "-.--":
-        scode += "y"
-    elif sentance == "--..":
-        scode += "z"
-    elif sentance == "-----":
-        scode += "0"
-    elif sentance == ".----":
-        scode += "1"
-    elif sentance == "..---":
-        scode += "2"
-    elif sentance == "...--":
-        scode += "3"
-    elif sentance == "....-":
-        scode += "4"
-    elif sentance == ".....":
-        scode += "5"
-    elif sentance == "-....":
-        scode += "6"
-    elif sentance == "--...":
-        scode += "7"
-    elif sentance == "---..":
-        scode += "8"
-    elif sentance == "----.":
-        scode += "9"
-        
-    return(scode)
-
-
-temp = ""
-full = ""
-count = 0
-for ch in code:
-    if ch == ".":
-        temp += "."
-    elif ch == "-":
-        temp += "-"
-    elif ch == "/":
-        count +=1
-        temp += "/"
-    elif ch == " ":
-        count +=1
-        full += decode(temp)
-        temp = ""
-    else:
-        print("error")
-full += decode(temp)
-print(full)
-numb = len(full)
-assert numb == count
+def morseCodeToLetters(code):
+    temp = ""
+    full = ""
+    for ch in code:
+        if ch == ".":
+            temp += "."
+        elif ch == "-":
+            temp += "-"
+        elif ch == "/":
+            temp += "/"
+        elif ch == " ":
+            full += decode(temp)
+            temp = ""
+    full += decode(temp)
+    assert len(code.split()) == len(full)
+    assert code.count('/') == full.count(" ")
+    return(full)
--- src/word_to_mos.py
+++ src/word_to_mos.py
@@ -1,120 +1,13 @@
-word = input("please enter a string ")
-count= 0
-num_ch = len(word)
+from dictionary import dic
 
-for ch in word:
-    if ch == " ":
-        count += 1
-        print("/", end=" ")
-    elif ch == "a":
-        count += 1
-        print(".-", end = " ")
-        count += 1
-    elif ch == "b":
-        print("-...", end = " ")
-        count +=1
-    elif ch == "c":
-        print("-.-.", end = " ")
-        count += 1
-    elif ch == "d":
-        print("-..", end = " ")
-        count += 1
-    elif ch == "e":
-        print(".", end = " ")
-        count += 1
-    elif ch == "f":
-        print("..-.", end = " ")
-        count += 1
-    elif ch == "g":
-        print("--.", end = " ")
-        count += 1
-    elif ch == "h":
-        print("....", end = " ")
-        count += 1
-    elif ch == "i":
-        print("..", end = " ")
-        count += 1
-    elif ch == "j":
-        print(".---", end = " ")
-        count += 1
-    elif ch == "k":
-        print("-.-", end = " ")
-        count += 1
-    elif ch == "l":
-        print(".-..", end = " ")
-        count += 1
-    elif ch == "m":
-        print("--", end = " ")
-        count += 1
-    elif ch == "n":
-        print("-.", end = " ")
-        count += 1
-    elif ch == "o":
-        print("---", end = " ")
-        count += 1
-    elif ch == "p":
-        print("-..-", end = " ")
-        count += 1
-    elif ch == "q":
-        print("--.-", end = " ")
-        count += 1
-    elif ch == "r":
-        print(".-.", end = " ")
-        count += 1
-    elif ch == "s":
-        print("...", end = " ")
-        count += 1
-    elif ch == "t":
-        print("-", end = " ")
-        count += 1
-    elif ch == "u":
-        print("..-", end = " ")
-        count += 1
-    elif ch == "v":
-        print("...-", end = " ")
-        count += 1
-    elif ch == "w":
-        print(".--", end = " ")
-        count += 1
-    elif ch == "x":
-        print("-..-", end = " ")
-        count += 1
-    elif ch == "y":
-        print("-.--", end = " ")
-        count += 1
-    elif ch == "z":
-        print("--..", end = " ")
-        count += 1
-    elif ch == "1":
-        print(".----", end = " ")
-        count += 1
-    elif ch == "2":
-        print("..---", end = " ")
-        count += 1
-    elif ch == "3":
-        print("...--", end = " ")
-        count += 1
-    elif ch == "4":
-        print("....-", end = " ")
-        count += 1
-    elif ch == "5":
-        print(".....", end = " ")
-        count += 1
-    elif ch == "6":
-        print("-....", end = " ")
-        count += 1
-    elif ch == "7":
-        print("--...", end = " ")
-        count += 1
-    elif ch == "8":
-        print("---..", end = " ")
-        count += 1
-    elif ch == "9":
-        print("----.", end = " ")
-        count += 1 
-    elif ch == "0":
-        print("-----", end = " ")
-        count += 1
-print("\n")
-
-assert count == num_ch
+def lettersToMorseCode(word):
+    final = ""
+    for ch in word:
+        for key in dic:
+            if ch == key:
+                final += dic[key]
+                final += " "
+    final.rstrip()  
+    assert len(word) == len(final.split())
+    assert word.count(" ") == final.count("/")
+    return(final)  
